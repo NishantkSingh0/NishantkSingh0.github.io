42658e90e47574175a71b95399992ce14ad75301
Nishant Singh <166206623+NishantkSingh0@users.noreply.github.com>
2025-07-11 19:47:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,9 +38,12 @@ $(document).ready(function () {
     });
     $('a[href*="#"]').on('click', function (e) {
         e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top,
-        }, 500, 'linear')
+        let target = $($(this).attr('href'));
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top - 10 
+            }, 500, 'swing'); 
+        }
     });
     $("#contact-form").submit(function (event) {
         emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
